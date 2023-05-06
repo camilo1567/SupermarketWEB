080a52754c4bb9af7fa52acf43a35676e1327d2f
CamiloHorta <jhorta@nethexa.com>
2023-05-06 09:12:52 -0500

    PayMode Edit

---
+++ Pages/PayModes/Edit.cshtml.cs
@@ -0,0 +1,71 @@
+using FirstWebApp.Data;
+using FirstWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstWebApp.Pages.PayModes
+{
+    public class EditModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public EditModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public PayMode PayMode { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.PayModes == null)
+            {
+                return NotFound();
+            }
+
+            var paymode = await _context.PayModes.FirstOrDefaultAsync(m => m.Id == id);
+            if (paymode == null)
+            {
+                return NotFound();
+            }
+            PayMode = paymode;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(PayMode).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PayModeExists(PayMode.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+
+            }
+
+            return RedirectToPage("./index");
+        }
+
+        private bool PayModeExists(int id)
+        {
+            return (_context.PayModes?.Any(c => c.Id == id)).GetValueOrDefault();
+        }
+    }
+}

b7d02ca1870d082b7ed764f6cc5534d7f10c34eb
CamiloHorta <jhorta@nethexa.com>
2023-05-06 18:25:42 -0500

    Detail Edit

---
+++ Pages/Details/Edit.cshtml.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using FirstWebApp.Data;
+using FirstWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstWebApp.Pages.Details
+{
+    public class EditModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public EditModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Detail Detail { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Details == null)
+            {
+                return NotFound();
+            }
+
+            var detail = await _context.Details.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (detail == null)
+            {
+                return NotFound();
+            }
+            Detail = detail;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Detail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DetailExists(Detail.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+
+            }
+
+            return RedirectToPage("./index");
+        }
+
+        private bool DetailExists(int id)
+        {
+            return (_context.Details?.Any(p => p.Id == id)).GetValueOrDefault();
+        }
+    }
+}

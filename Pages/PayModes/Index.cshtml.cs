065e1eaf92b41e39bf0fc51e4fba1981f7b23485
CamiloHorta <jhorta@nethexa.com>
2023-05-06 08:58:57 -0500

    PayMode Index

--- Pages/PayModes/Index.cshtml.cs
+++ Pages/PayModes/Index.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FirstWebApp.Models;
+using FirstWebApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace FirstWebApp.Pages.PayModes
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly SupermarketContext _context;
+
+        public IndexModel(SupermarketContext context)
         {
+            _context = context;
+        }
+
+        public IList<PayMode> PayModes { get; set; } = default!;
+        public async Task OnGetAsync()
+        {
+            if (_context.PayModes != null)
+            {
+                PayModes = await _context.PayModes.ToListAsync();
+            }
         }
     }
 }
